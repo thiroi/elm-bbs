49c16f793b70775dc51ca26cd4ba2d50e25ffc26
hiroi <hiroi_t@fringe81.com>
2019-03-04 16:54:49 +0900

    add detail screen

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,28 @@
 port module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
+import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
+import Json.Decode as Decode exposing (Decoder, Error)
+import Url
+
+
+
+---- PROGRAM ----
+
+
+main : Program Decode.Value Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
 
 
 
@@ -11,22 +30,61 @@ import Html.Events exposing (onClick, onInput)
 
 
 type alias Model =
-    { inputTitle : String
+    { key : Nav.Key
+    , url : Url.Url
+    , inputTitle : String
     , inputName : String
     , threads : List Thread
     }
 
 
 type alias Thread =
-    { title : String
+    { id : Int
+    , title : String
     , owner : String
-    , comments : List String
+    , comments : List Comment
     }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( Model "" "" [ Thread "なにはなそう" "ぼぶ" [ "すごいこめんと", "最強のコメント" ], Thread "あたらしいはなし" "まっと" [ "すごいこめんと", "そこそこのコメント" ] ], Cmd.none )
+type alias Comment =
+    { user : String
+    , message : String
+    }
+
+
+flagsDecoder : Decoder (List Thread)
+flagsDecoder =
+    Decode.list threadDecoder
+
+
+threadDecoder : Decoder Thread
+threadDecoder =
+    Decode.map4 Thread
+        (Decode.field "id" Decode.int)
+        (Decode.field "title" Decode.string)
+        (Decode.field "owner" Decode.string)
+        (Decode.field "comments" (Decode.list commentDecoder))
+
+
+commentDecoder : Decoder Comment
+commentDecoder =
+    Decode.map2 Comment
+        (Decode.field "user" Decode.string)
+        (Decode.field "message" Decode.string)
+
+
+init : Decode.Value -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    let
+        initialModel =
+            case Decode.decodeValue flagsDecoder flags of
+                Err _ ->
+                    [ Thread 1 "なにはなそう" "ぼぶ" [ Comment "Sam" "すごいこめんと", Comment "えんどう" "最強のコメント" ], Thread 2 "あたらしいはなし" "まっと" [ Comment "すーちゃん" "すごいこめんと", Comment "名無し" "そこそこのコメント" ] ]
+
+                Ok model ->
+                    model
+    in
+    ( Model key url "" "" initialModel, Cmd.none )
 
 
 
@@ -39,6 +97,8 @@ type Msg
     | UpdateInputName String
     | MakeThread
     | SyncLocalStorage (List Thread)
+    | LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -54,18 +114,57 @@ update msg model =
             ( model, Cmd.none )
 
         MakeThread ->
-            ( { model | inputTitle = "", inputName = "" }, portSetLocalStorage ( "threads", Thread model.inputTitle model.inputName [] :: model.threads ) )
+            ( { model | inputTitle = "", inputName = "" }, portSetLocalStorage ( "threads", Thread (getNewId model) model.inputTitle model.inputName [] :: model.threads ) )
 
         SyncLocalStorage newThreads ->
             ( { model | threads = newThreads }, Cmd.none )
+
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }
+            , Cmd.none
+            )
+
+
+getNewId : Model -> Int
+getNewId model =
+    List.length model.threads + 1
 
 
 
 ---- VIEW ----
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
+    if String.contains "thread" (Url.toString model.url) then
+        { title = "Thread Detail"
+        , body =
+            [ detailView model
+            ]
+        }
+
+    else
+        { title = "All Thread"
+        , body =
+            [ allView model
+            ]
+        }
+
+
+
+----- 一覧用
+
+
+allView : Model -> Html Msg
+allView model =
     div [ class "center" ]
         [ div [ class "inputArea" ]
             [ div []
@@ -82,28 +181,13 @@ view model =
         , div [ class "threads" ]
             [ tableView model.threads
             ]
-        , div [] [ text <| "current user id = " ++ model.inputTitle ]
         ]
-
-
-type alias MyListItem =
-    { title : String
-    , owner : String
-    , numberOfComments : Int
-    }
-
-
-myList : List MyListItem
-myList =
-    [ { title = "ナニヤルGW", owner = "someone", numberOfComments = 15 }
-    , { title = "ナニヤル新年", owner = "anyone", numberOfComments = 32 }
-    ]
 
 
 toTableRow : Thread -> Html Msg
 toTableRow threadItem =
     tr []
-        [ td [] [ text threadItem.title ]
+        [ td [] [ viewLink threadItem.title ("/thread/" ++ String.fromInt threadItem.id) ]
         , td [] [ text threadItem.owner ]
         , td [] [ text (String.fromInt (List.length threadItem.comments)) ]
         ]
@@ -123,18 +207,74 @@ tableView threadList =
         )
 
 
+viewLink : String -> String -> Html msg
+viewLink label path =
+    div [] [ a [ href path ] [ text label ] ]
 
----- PROGRAM ----
 
 
-main : Program () Model Msg
-main =
-    Browser.element
-        { view = view
-        , init = \_ -> init
-        , update = update
-        , subscriptions = subscriptions
-        }
+----- コメント用
+
+
+detailView : Model -> Html Msg
+detailView model =
+    let
+        thread =
+            findTargetThread model
+    in
+    div []
+        [ div
+            []
+            [ text thread.title ]
+        , commentsView thread.comments
+        , div
+            []
+            [ viewLink "back" "/" ]
+        ]
+
+
+commentsView : List Comment -> Html Msg
+commentsView commentList =
+    table
+        [ class "center" ]
+        (List.map toCommentCard commentList)
+
+
+toCommentCard : Comment -> Html msg
+toCommentCard comment =
+    div []
+        [ h1 [] [ text comment.user ]
+        , text comment.message
+        ]
+
+
+findTargetThread : Model -> Thread
+findTargetThread model =
+    let
+        maybeId =
+            String.split "/" (Url.toString model.url)
+                |> List.reverse
+                |> List.head
+
+        targetId =
+            case maybeId of
+                Nothing ->
+                    "999999"
+
+                Just id ->
+                    id
+
+        targetModelMaybe =
+            model.threads
+                |> List.filter (\thread -> String.fromInt thread.id == targetId)
+                |> List.head
+    in
+    case targetModelMaybe of
+        Nothing ->
+            Thread 99999999 "なにはなそう" (Url.toString model.url) [ Comment "Sam" "すごいこめんと", Comment "えんどう" "最強のコメント" ]
+
+        Just thread ->
+            thread
 
 
 
--- src/index.js
+++ src/index.js
@@ -2,32 +2,33 @@ import "./main.css";
 import { Elm } from "./Main.elm";
 import registerServiceWorker from "./registerServiceWorker";
 
+console.log(localStorage.getItem("threads"));
+
 var app = Elm.Main.init({
-  node: document.getElementById("root")
+  node: document.getElementById("root"),
+  flags: JSON.parse(localStorage.getItem("threads"))
 });
 
 app.ports.portSetLocalStorage.subscribe(req => {
-  console.log("TESTだよ");
-  console.log(req[1]);
-  var data = convertThreads(req[1]);
-  console.log(data);
-  app.ports.syncElm.send(data);
+  const newItem = req[1];
+  localStorage.setItem("threads", JSON.stringify(newItem));
+  app.ports.syncElm.send(newItem);
 });
 
-function convertThreads(threads) {
-  var result = new Array();
-  threads.forEach(element => {
-    result.push(convert(element));
-  });
+// function convertThreads(threads) {
+//   var result = new Array();
+//   threads.forEach(element => {
+//     result.push(convert(element));
+//   });
 
-  return result;
-}
+//   return result;
+// }
 
-function convert(thread) {
-  return {
-    title: thread.title,
-    owner: thread.owner,
-    comments: thread.comments
-  };
-}
+// function convert(thread) {
+//   return {
+//     title: thread.title,
+//     owner: thread.owner,
+//     comments: thread.comments
+//   };
+// }
 registerServiceWorker();
